5a2f6e6b83cdf35baf9ebdcfdf8e7f801e29b9e3
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-22 16:06:21 +0000

    Created User repo interface

--- src/main/java/com/codeclan/files_folders_hw/repositories/UserRepository.java
+++ src/main/java/com/codeclan/files_folders_hw/repositories/UserRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.files_folders_hw.repositories;
 
-public class UserRepository {
+import com.codeclan.files_folders_hw.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
