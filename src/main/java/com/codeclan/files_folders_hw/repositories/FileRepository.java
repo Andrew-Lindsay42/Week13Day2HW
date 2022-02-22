0ff20e9049b541f0b7086de4ceb90efde39d4bef
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-22 16:05:07 +0000

    Created File repo interface

--- src/main/java/com/codeclan/files_folders_hw/repositories/FileRepository.java
+++ src/main/java/com/codeclan/files_folders_hw/repositories/FileRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.files_folders_hw.repositories;
 
-public class FileRepository {
+import com.codeclan.files_folders_hw.models.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Long> {
 }
