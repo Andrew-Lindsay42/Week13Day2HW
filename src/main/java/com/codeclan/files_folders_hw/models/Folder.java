fbcab8d1fd2608fbbd3658dce9fa63ce3eb2cab6
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-22 15:44:32 +0000

    Basic folder class

--- src/main/java/com/codeclan/files_folders_hw/models/Folder.java
+++ src/main/java/com/codeclan/files_folders_hw/models/Folder.java
@@ -1,4 +1,51 @@
 package com.codeclan.files_folders_hw.models;
 
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Folder {
+
+    private Long id;
+    private String title;
+    private List<Files> files;
+    private User user;
+
+    public Folder(String title, User user) {
+        this.title = title;
+        this.user = user;
+        this.files = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Files> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<Files> files) {
+        this.files = files;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
